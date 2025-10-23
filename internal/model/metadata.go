26f2ed702ab24ccffc4c33b2bd8fbf4e2cf71bba
lew <lew@ily.rs>
2025-10-23 21:25:15 +0100

    feat(internal/model): added some helpers to Metadata

--- internal/model/metadata.go
+++ internal/model/metadata.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"strings"
+	"time"
+)
 
 // Metadata captures descriptive fields persisted alongside snippet contents.
 type Metadata struct {
@@ -12,6 +15,23 @@ type Metadata struct {
 	Tags        string
 }
 
+func (m *Metadata) Formatted(name string) string {
+	switch name {
+	case "Date":
+		return m.DateStr()
+	case "Type":
+		return m.TypeStr()
+	case "Name":
+		return m.NameStr()
+	case "Tags":
+		return m.TagsStr()
+	case "Desc":
+		return m.DescStr()
+	default:
+		return ""
+	}
+}
+
 func (m *Metadata) TypeIcon() string {
 	switch m.Type {
 	case "url":
@@ -19,4 +39,30 @@ func (m *Metadata) TypeIcon() string {
 	default:
 		return "txt"
 	}
+}
+
+func (m *Metadata) TypeStr() string {
+	return m.TypeIcon()
+}
+
+func (m *Metadata) DateStr() string {
+	return m.Modified.UTC().Format("02 Jan 15:04")
+}
+
+func (m *Metadata) NameStr() string {
+	return m.Key
+}
+
+func (m *Metadata) TagsStr() string {
+	if len(m.Tags) == 0 {
+		return ""
+	}
+	// Split CSV and prefix with @.
+	// one,two,three -> @one @two @three
+	split := strings.Split(m.Tags, ",")
+	return "@" + strings.Join(split, " @")
+}
+
+func (m *Metadata) DescStr() string {
+	return m.Description
 }
